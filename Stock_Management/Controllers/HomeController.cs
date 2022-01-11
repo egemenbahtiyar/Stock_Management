f61e1166c85acf31e3abb8bf0c4bdb73e0720f4a
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-01-12 01:17:20 +0300

    Home Index was arranged with viewmodel.

--- Stock_Management/Controllers/HomeController.cs
+++ Stock_Management/Controllers/HomeController.cs
@@ -12,15 +12,23 @@ namespace Stock_Management.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Ecommerce_Stock_ManagementContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,Ecommerce_Stock_ManagementContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(HomeIndexViewModel Hvm)
         {
-            return View();
+            Hvm.NoOfCategories = _context.Category.Count();
+            Hvm.NoOfCustomers = _context.Customer.Count();
+            Hvm.NoOfEmployees = _context.Employee.Count();
+            Hvm.NoOfOrders = _context.Order.Count();
+            Hvm.NoOfProducts = _context.Product.Count();
+            Hvm.NoOfStorages = _context.Storage.Count();
+            return View(Hvm);
         }
 
         public IActionResult Contact()
---
+++ Stock_Management/Models/HomeIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock_Management.Models
+{
+    public class HomeIndexViewModel
+    {
+        public int NoOfOrders { get; set; }
+        public int NoOfProducts { get; set; }
+        public int NoOfCategories { get; set; }
+        public int NoOfCustomers { get; set; }
+        public int NoOfStorages { get; set; }
+        public int NoOfEmployees { get; set; }
+    }
+}
